3f2d509b27f7e53985f8634d0eba651bf9369e88
gbotas <gbotas@uchicago.edu>
2025-04-11 22:47:45 +0000

    dicewithviewsv6

--- dice.rb
+++ dice.rb
@@ -16,45 +16,59 @@ end
 
 
 get("/dice/2/6") do
-  dice1= rand(1..6)
-  dice2 = rand(1..6)
-  sum = dice1 + dice2
 
-  @outcome = "You rolled a #{dice1} and a #{dice2} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    dice= rand(1..6)
+
+    @rolls.push(dice)
+
+  end 
 
   erb(:two_six)
 end 
 
 get("/dice/2/10") do
-  dice1= rand(1..10)
-  dice2 = rand(1..10)
-  sum= dice1 + dice2
 
-  @outcome = "Your rolled a #{dice1} and a #{dice2} for a total of #{sum}."
+  @rolls = []
+
+  2.times do 
+    dice= rand(1..10)
+
+    @rolls.push(dice)
+
+  end 
+  
 
   erb(:two_ten)
 
 end 
 
 get("/dice/1/20") do
-  @dice1= rand(1..20)
+  
+  @rolls = []
 
-  @outcome= "You have rolled a #{@dice1}."
+  1.times do 
+    dice= rand(1..20)
+
+    @rolls.push(dice)
+
+  end 
 
   erb(:one_twenty)
 
 end 
 
 get("/dice/5/4") do
-  dice1= rand(1..4)
-  dice2 = rand(1..4)
-  dice3 = rand(1..4)
-  dice4 = rand(1..4)
-  dice5 = rand(1..4)
-  
-  sum= dice1 + dice2 + dice3 + dice4 + dice5
+  @rolls= []
 
-  @outcome = "You have rolled a #{dice1}, #{dice2}, #{dice3}, #{dice4}, and a #{dice5} for a total of #{sum}"
+  5.times do
+    dice= rand(1..4)
+
+    @rolls.push(dice)
+
+  end 
 
   erb(:five_four)
 
